9fe2943162c4a19eb93084ea53147ff318426e6f
ibayjimwell <ibayjimwell@gmail.com>
2026-01-03 07:37:19 +0000

    Configured for production

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import connectDB from './database/mongodb.js';
 import trendingRouter from './routes/trending.route.js';
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
